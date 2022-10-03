a3c5eaf1d3035381763c5e8737ec8c972421efac
Kyle Brackman <kyle.a.brackman@gmail.com>
2022-10-02 22:39:02 -0600

    commit tree b1c30a4e54b5457b6a908a58180f0c483692a6a5

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
-// code your solution here
+function superbowlWin(record) {
+    const win = record.find(obj => obj.result === "W")
+    if(win) {
+        return win.year
+    }
+    else {
+        return undefined
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function superbowlWin(record) {
+//     const win = record.find(obj => obj.result === "W")
+//     if(win) {
+//         return win.year
+//     }
+// }
+
+
+// function superbowlWin(item) {
+//     const win = item.find((x) => x.result === "W");
+//     if (win) {
+//         return win.year
+//     }
+//     else {
+//         return undefined
+//     }
+// }
